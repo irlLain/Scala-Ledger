bf4c8e6b7ed8053a4f730ca7a0bacc31ae3f968d
Graves <morgan.graves@accenture.com>
2026-04-07 12:57:59 +0100

    Fixed code so all category tests pass.

--- src/main/scala/ledger/domain/category.scala
+++ src/main/scala/ledger/domain/category.scala
@@ -1,6 +1,6 @@
 package ledger.domain
 
 case class Category(value: String, amount: BigDecimal = BigDecimal(0)) {
-  def isIncome: Boolean = amount > 0
+  def isIncome: Boolean = amount >= 0
   def isExpense: Boolean = amount < 0
 }
